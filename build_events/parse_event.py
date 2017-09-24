e4fcc21f79aef980666fb2bc903d1fffc26aae8f
Matt Sachtler <matt.sachtler@gmail.com>
2017-09-23 18:20:12 -0700

    Did some work parsing each message type.
    I better understand the build events. This demos how to dispatch out specific
    message types.
    
    From here, building the graph seems like the next step.

--- build_events/parse_event.py
+++ build_events/parse_event.py
@@ -1,10 +1,9 @@
-from build_events.build_event_stream_pb2 import (
-    BuildEvent,
-)
+import build_events.build_event_stream_pb2 as bes
+from google.protobuf import message as _message
 from google.protobuf.internal.decoder import _DecodeVarint32
 
 def decode_from_str(data):
-    event = BuildEvent()
+    event = bes.BuildEvent()
     event.ParseFromString(data)
     return event
 
@@ -17,7 +16,7 @@ def from_prefixed_bin(data):
     # decoding that many bytes at a time.
     next_pos, pos = 0, 0
     while pos < len(data):
-        event = BuildEvent()
+        event = bes.BuildEvent()
         next_pos, pos = decoder(data, pos)
         event.ParseFromString(data[pos:pos + next_pos])
         messages.append(event)
@@ -25,8 +24,60 @@ def from_prefixed_bin(data):
 
     return messages
 
-
 def print_stderr(event):
     if event.progress.stderr:
         print event.progress.stderr
 
+def handle(event):
+    process(event)
+    print_stderr(event)
+
+def process(event):
+    # Dispatch messages based on their type.
+    msg_type = event.WhichOneof("payload")
+    if msg_type == "progress":
+        print "progress"
+    elif msg_type == "aborted":
+        print "aborted"
+    elif msg_type == "started":
+        print "started"
+    elif msg_type == "command_line":
+        print "command_line"
+    elif msg_type == "options_parsed":
+        print "options_parsed"
+    elif msg_type == "workspace_status":
+        print "workspace_status"
+    elif msg_type == "fetch":
+        print "fetch"
+    elif msg_type == "configuration":
+        print "configuration"
+    elif msg_type == "expanded":
+        print "expanded"
+    elif msg_type == "configured":
+        print "configured"
+        print_target_configured(event)
+    elif msg_type == "action":
+        print "action"
+    elif msg_type == "named_set_of_files":
+        print "named_set_of_files"
+    elif msg_type == "completed":
+        print "completed"
+    elif msg_type == "test_result":
+        print "test_result"
+    elif msg_type == "test_summary":
+        print "test_summary"
+    elif msg_type == "finished":
+        print "finished"
+    else:
+        print "unknown",
+        print msg_type
+
+    for c in event.children:
+        print "child", c.WhichOneof("id")
+
+
+def print_target_configured(event):
+    print event.HasField("configured")
+    print "---"
+    print event.id.target_configured.label
+    print "---"
--- parse_prefixed_bin.py
+++ parse_prefixed_bin.py
@@ -1,6 +1,6 @@
 from build_events.parse_event import (
         from_prefixed_bin,
-        print_stderr,
+        handle,
 )
 import sys
 
@@ -11,5 +11,5 @@ data = fd.read()
 events = from_prefixed_bin(data)
 
 for event in events:
-    print_stderr(event)   
+    handle(event)   
 
